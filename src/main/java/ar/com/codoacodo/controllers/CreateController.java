e63cf870f0f7053c3a68b79aa7f5603fa8151a23
maximiliano Viapiano <maximiliano.viapiano@bue.edu.ar>
2022-07-11 00:20:32 -0300

    clase 35

--- src/main/java/ar/com/codoacodo/controllers/CreateController.java
+++ src/main/java/ar/com/codoacodo/controllers/CreateController.java
@@ -1,65 +1,36 @@
 package ar.com.codoacodo.controllers;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Statement;
 
-import ar.com.codoacodo.connection.AdministradorDeConexiones;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/*herencia*/
-//create controler es hijo de :
+import ar.com.codoacodo.daos.ProductoDAO;
+
+/*HERENCIA*/
+//Create Controller es hijo de :
 @WebServlet("/CreateController")
 public class CreateController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//recibe los datos del front 
-		String nombre = req.getParameter("nombre"); 
-		String precio = req.getParameter("precio");  //convertir en float
-		String fechaCreacion= "";   // la vamos a asignar nosotros 
-		String imagen = req.getParameter("imagen"); 
-		String codigo = req.getParameter("codigo"); 
-		
-		Connection con = AdministradorDeConexiones.getConnection(); 
-		if(con !=null) {
-			String sql = "INSERT INTO producto (nombre, precio, fecha_creacion, imagen, codigo) ";
-				   sql+= "VALUES ('"+nombre+"',"+precio+", curdate(), '"+imagen+"', '"+codigo+"')"; 
-			
-		   try {
-				Statement st = con.createStatement(); 
-				st.execute(sql); 
-				
-				// cierre dela conexion 
-				con.close();
-				// no funciona :-(((
-				//getServletContext().getRequestDispatcher("/api/ListadoController").forward(req, resp);
-				// cambio la forma de redireccionar desues del alta 
-				
-				resp.sendRedirect(req.getContextPath()+"/api/ListadoController");
-		   } catch(Exception e) {
-			   e.printStackTrace();
-		   } 
-			
-		}
-		
-		
-		
-		
-		
-		//en req viene los datos que manda el formuario html 
-		//clave = valor
-		
-		//pedir una conexion: AdministradordeConexiones.getconection()
-		
-		//insert en la db > sql insert into 
 
+		String nombre = req.getParameter("nombre");
+		String precio = req.getParameter("precio");
+		String imagen = req.getParameter("imagen");
+		String codigo = req.getParameter("codigo");
 		
+		//crear ProductoDAO
+		ProductoDAO dao = new ProductoDAO();
 		
+		//ejecutar el metodo crearProducto(parametros...)
+		dao.crearProducto(nombre, Float.parseFloat(precio), imagen, codigo);
+		
+		//ctrl+shit+o
+		//ir a la siguiente pagina
+		resp.sendRedirect(req.getContextPath()+"/api/ListadoController");
 	}
-	
 }
